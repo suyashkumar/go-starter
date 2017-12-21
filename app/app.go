3cdd85348c4b931a641d428e134d4583c883f28e
Suyash Kumar <suyashkumar2003@gmail.com>
2017-12-20 20:22:58 -0500

    Configure logging

--- app/app.go
+++ app/app.go
@@ -6,6 +6,7 @@ import (
 
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/julienschmidt/httprouter"
+	"github.com/sirupsen/logrus"
 	"github.com/suyashkumar/go-starter/config"
 )
 
@@ -17,6 +18,7 @@ type app struct {
 	router *httprouter.Router
 }
 
+// New creates and initializes a new web service App
 func New() (App, error) {
 	// Create router and attach routes
 	r := httprouter.New()
@@ -29,10 +31,12 @@ func New() (App, error) {
 
 func (a *app) Run() {
 	if config.Get(config.IsDev) != "" {
-		fmt.Printf("Listening on port :%s\n", config.Get(config.Port))
-		err := http.ListenAndServe(":"+config.Get(config.Port), a.router)
+		p := config.Get(config.Port)
+		logrus.WithField("port", p).Info("Server listening")
+		err := http.ListenAndServe(fmt.Sprintf(":%s", p), a.router)
 		panic(err)
 	} else {
+		logrus.Info("Listening on :443")
 		err := http.ListenAndServeTLS(":443", config.Get(config.CertKey), config.Get(config.PrivKey), a.router)
 		panic(err)
 		//TODO(suyash): Add redirect to https
